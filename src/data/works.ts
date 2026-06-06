d879db151bc963a934468ff96e14e7fec903cce4
前城真理 <maeshiromari@maeshiromarinoMacBook-Air-2.local>
2026-06-06 16:39:05 +0900

    Add Eden Creative Team portfolio site

---
+++ src/data/works.ts
@@ -0,0 +1,63 @@
+export type Work = {
+  id: string
+  title: string
+  image: string
+  href?: string
+  imageFit?: 'cover' | 'contain'
+  imagePosition?: string
+}
+
+export const works: Work[] = [
+  {
+    id: 'imtrix',
+    title: 'IMTRIX LP',
+    image: '/imtrix.png',
+    href: 'https://imtrix.jp/',
+    imageFit: 'cover',
+    imagePosition: 'center top',
+  },
+  {
+    id: 'zebra',
+    title: 'ZEBRA HP',
+    image: '/zebra.png',
+    href: 'https://www.zebra-company.jp/',
+    imageFit: 'cover',
+    imagePosition: 'center top',
+  },
+  {
+    id: 'ksk',
+    title: 'KSK HP',
+    image: '/ksk.png',
+    href: 'https://ksk-logistics.co.jp/',
+    imageFit: 'cover',
+    imagePosition: 'center center',
+  },
+  {
+    id: 'flyer-apparel',
+    title: 'オーダーアパレル商品案内 チラシ',
+    image: '/flyer-apparel.png',
+    imageFit: 'cover',
+    imagePosition: 'center top',
+  },
+  {
+    id: 'flyer-dtf',
+    title: 'DTFプリント訴求 チラシ',
+    image: '/flyer-dtf.png',
+    imageFit: 'cover',
+    imagePosition: 'center top',
+  },
+  {
+    id: 'flyer-original-print',
+    title: 'オリジナルプリントサービス チラシ',
+    image: '/flyer-original-print.png',
+    imageFit: 'cover',
+    imagePosition: 'center top',
+  },
+  {
+    id: 'flyer-school',
+    title: 'オリジナルTシャツ チラシ',
+    image: '/flyer-school.png',
+    imageFit: 'cover',
+    imagePosition: 'center top',
+  },
+]
